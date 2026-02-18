2ddbd607ace61aa363d0c13fd681643dc6f3fc6f
Aabirah <aabirah04@gmail.com>
2026-02-18 15:50:23 +0530

    Add files via upload

---
+++ app.py cnn.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+# Load trained model
+model = tf.keras.models.load_model("cnn_model.h5")
+
+st.title("🐱🐶 CNN Image Classification App")
+st.write("Upload an image to classify as Cat or Dog")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    img = image.resize((128, 128))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+
+    if prediction[0][0] > 0.5:
+        st.success("Prediction: 🐶 Dog")
+    else:
+        st.success("Prediction: 🐱 Cat")
